b25575643fd2710f6cac439b612d9292d7059e81
Irene Papakonstantinou <eirhnh@gmail.com>
2012-10-18 09:13:02 +0100

    more tests

--- src/test/java/LowestCommonAncestorTest.java
+++ src/test/java/LowestCommonAncestorTest.java
@@ -1,5 +1,6 @@
-import org.fest.assertions.Assertions;
 import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class LowestCommonAncestorTest {
     Node tree = new Node(5, new Node(2, new Node(35, Node.leaf(9), Node.leaf(4)), new Node(17, Node.leaf(73), Node.leaf(29))),
@@ -16,29 +17,29 @@ public class LowestCommonAncestorTest {
 
     @Test
     public void testDFS() {
-        Node lca = LowestCommonAncestorDFS.lowestCommonAncestor(tree, 1, 13);
-        Assertions.assertThat(lca.val).isEqualTo(8);
-
+        assertThat(LowestCommonAncestorDFS.lowestCommonAncestor(tree, 1, 13).val).isEqualTo(8);
+        assertThat(LowestCommonAncestorDFS.lowestCommonAncestor(tree, 1, 1000)).isEqualTo(null);
     }
 
     @Test
     public void testBFS() {
-        LowestCommonAncestorBFS lowestCommonAncestorBFS = new LowestCommonAncestorBFS();
-        Integer lca = lowestCommonAncestorBFS.lowestCommonAnestor(tree, 1, 13);
-        Assertions.assertThat(lca).isEqualTo(8);
+        LowestCommonAncestorBFS lca = new LowestCommonAncestorBFS();
+        assertThat(lca.lowestCommonAnestor(tree, 1, 13)).isEqualTo(8);
+        assertThat(lca.lowestCommonAnestor(tree, 35, 17)).isEqualTo(2);
+        assertThat(lca.lowestCommonAnestor(tree, 2, 12)).isEqualTo(5);
+        assertThat(lca.lowestCommonAnestor(tree, 2, 1000)).isEqualTo(null);
+        assertThat(lca.lowestCommonAnestor(tree, 5, 1)).isEqualTo(5);
     }
 
     @Test
     public void testRecurse() {
-        LowestCommonAncestorRecurse lcaRec = new LowestCommonAncestorRecurse();
-        Node lca = lcaRec.lowestCommonAnestor(tree, 1, 13);
-        Assertions.assertThat(lca.val).isEqualTo(8);
+        LowestCommonAncestorRecurse lca = new LowestCommonAncestorRecurse();
+        assertThat(lca.lowestCommonAnestor(tree, 1, 13).val).isEqualTo(8);
+        assertThat(lca.lowestCommonAnestor(tree, 35, 17).val).isEqualTo(2);
+        assertThat(lca.lowestCommonAnestor(tree, 2, 12).val).isEqualTo(5);
+        assertThat(lca.lowestCommonAnestor(tree, 2, 1000)).isEqualTo(null);
+        assertThat(lca.lowestCommonAnestor(tree, 5, 1).val).isEqualTo(5);
     }
-    @Test
-    public void testRecurseMemo() {
-        LowestCommonAncestorRecurseMemoize lcaRecMemo = new LowestCommonAncestorRecurseMemoize();
-        Node lca = lcaRecMemo.lowestCommonAnestor(tree, 1, 13);
-        Assertions.assertThat(lca.val).isEqualTo(8);
-    }
+
 
 }
